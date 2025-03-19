1d3d35c15e4a1b056746c599e920ccf0f1032184
Robert <128862496+robertib24@users.noreply.github.com>
2025-03-19 21:01:55 +0200

    ~solved - 2 ways

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,13 +1,30 @@
-class Solution: # ideal O(n)
+# Bruteforce
+class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+        res = nums^2 * len(list)
 
-        prefix = 1
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
+            prod = 1
+            for j in range(len(nums)):
+                if i == j:
+                    continue    
+                prod *= nums[j]
+            
+            res[i] = prod
+        return res
+
+# O(n)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+
+        pre = 1
+        for i in range(len(nums)):
+            res[i] = pre
+            pre *= nums[i]
+        post = 1
+
         for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
+            res[i] *= post
+            post *= nums[i]
         return res
