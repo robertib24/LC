31cc3aca5cdd8f295681255c6c3c4d258cc9888c
Robert <128862496+robertib24@users.noreply.github.com>
2025-10-09 23:57:14 +0300

    ~solved

---
+++ 3494. Find the Minimum Amount of Time to Brew Potions.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minTime(self, skill: List[int], mana: List[int]) -> int:
+        sumSkill = sum(skill)
+        prevWizardDone = sumSkill * mana[0]
+        
+        for j in range(1, len(mana)):
+            prevPotionDone = prevWizardDone
+            
+            for i in range(len(skill) - 2, -1, -1):
+                prevPotionDone -= skill[i + 1] * mana[j - 1]
+                prevWizardDone = max(prevPotionDone, 
+                                     prevWizardDone - skill[i] * mana[j])
+            
+            prevWizardDone += sumSkill * mana[j]
+        
+        return prevWizardDone
